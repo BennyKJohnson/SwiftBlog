b2dda1a6ad99dc97886c09d4d7f3e48ea5a7687d
Benjamin Johnson <bendude@live.com.au>
2016-02-09 17:08:05 +1100

    Initial Commit

---
+++ SwiftBlog/SwiftBlog.h
@@ -0,0 +1,19 @@
+//
+//  SwiftBlog.h
+//  SwiftBlog
+//
+//  Created by Benjamin Johnson on 9/02/2016.
+//  Copyright © 2016 Benjamin Johnson. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for SwiftBlog.
+FOUNDATION_EXPORT double SwiftBlogVersionNumber;
+
+//! Project version string for SwiftBlog.
+FOUNDATION_EXPORT const unsigned char SwiftBlogVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftBlog/PublicHeader.h>
+
+
